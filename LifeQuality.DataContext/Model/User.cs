0878d9ff9f73e14b7c92fb6d76fd31d13c961d06
N.Lysenko <nazarii.lysenko.pz.2020@lpnu.ua>
2023-12-09 20:22:55 +0200

    Cleanup

--- LifeQuality.DataContext/Model/User.cs
+++ LifeQuality.DataContext/Model/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LifeQuality.DataContext.Model
+﻿namespace LifeQuality.DataContext.Model
 {
     public class User: EntityWithUpdateCreateFields
     {
